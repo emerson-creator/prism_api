cabfb1613d0f0f71f72e521118a0429548af190b
Emerson Creator <emerson.albornoz@correounivalle.edu.co>
2026-05-19 20:48:44 -0500

    feat(db): set up Prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({
+      adapter,
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'error', 'warn']
+          : ['error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Prisma connected to the database');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('Prisma disconnected from the database');
+  }
+
+  async cleanDatabase() {
+    if (process.env.NODE_ENV === 'production') {
+      throw new Error('Cannot clean database in production environment');
+    }
+    const models = Reflect.ownKeys(this).filter(
+      (key) => typeof key === 'string' && !key.startsWith('_'),
+    );
+
+    return Promise.all(
+      models.map((model) => {
+        if (typeof model === 'string') {
+          return this[model].deleteMany();
+        }
+      }),
+    );
+  }
+}
