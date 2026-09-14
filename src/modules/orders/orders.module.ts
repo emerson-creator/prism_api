c0d93ee874af927c3919512edcd2b0c7cc6861c2
emerson-creator <emerson.albornoz@correounivalle.edu.co>
2026-09-14 06:41:56 -0500

    feat(orders): register order cleanup task in module

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { MailModule } from '../mail/mail.module';
+import { OrdersCleanupTask } from './order-cleanup.task';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersCleanupTask],
   imports: [MailModule],
 })
 export class OrdersModule {}
